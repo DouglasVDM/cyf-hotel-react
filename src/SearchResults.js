1da76e3438efcccac3ea7c611134a9c5bb1dd5f5
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-23 23:04:06 +0200

    remove BookingTable.js file

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,8 +1,62 @@
 import React from "react";
-import BookingTable from "./BookingTable";
+import FakeBookings from "./data/fakeBookings.json";
 
 function SearchResults() {
-  return <BookingTable />;
+  return (
+    <div className="table-responsive">
+      <table className="table">
+        <caption>List of Customers</caption>
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Surname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Room ID</th>
+            <th scope="col">Check in Date</th>
+            <th scope="col">Check out Date</th>
+            <th scope="col">Number Of Nights</th>
+          </tr>
+        </thead>
+        {FakeBookings.map(data => {
+          return (
+            <tbody key={data.id}>
+              <tr>
+                <th scope="col">{data.id}</th>
+                <th scope="col">{data.title}</th>
+                <th scope="col">{data.firstName}</th>
+                <th scope="col">{data.surname}</th>
+                <th scope="col">{data.email}</th>
+                <th scope="col">{data.roomId}</th>
+                <th scope="col">{data.checkInDate}</th>
+                <th scope="col">{data.checkOutDate}</th>
+                <th scope="col">
+                  {getDifferenceInDays(data.checkInDate, data.checkOutDate)}
+                </th>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+    </div>
+  );
+}
+
+function getDifferenceInDays(start, end) {
+  const date1 = new Date(start);
+  const date2 = new Date(end);
+
+  // One day in milliseconds
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  // Calculating the time difference between two dates
+  const diffInTime = date2.getTime() - date1.getTime();
+
+  // Calculating the no. of days between two dates
+  const diffInDays = Math.round(diffInTime / oneDay);
+
+  return diffInDays;
 }
 
 export default SearchResults;
